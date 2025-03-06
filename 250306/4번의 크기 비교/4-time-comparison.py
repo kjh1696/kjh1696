a4d8c0d67a755c4396118fb8d0323e43d8e2936b
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-06 20:40:10 +0900

    Title: 4번의 크기 비교, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,14 @@
+num1 = int(input())
+
+numbers = input()
+numbers = numbers.split(" ")
+
+numb2 = int(numbers[0])
+numb3 = int(numbers[1])
+numb4 = int(numbers[2])
+numb5 = int(numbers[3])
+
+print(int(num1 > numb2))
+print(int(num1 > numb3))
+print(int(num1 > numb4))
+print(int(num1 > numb5))
