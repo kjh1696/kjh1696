621b42113e3a3b0f09b90f6a966a2cac35250ccd
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-06 20:37:47 +0900

    Title: 4가지 관계연산자, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,10 @@
+numbers = input()
+numbers = numbers.split()
+
+num1 = int(numbers[0])
+num2 = int(numbers[1])
+
+print(int(num1 >= num2))
+print(int(num1 > num2))
+print(int(num2 >= num1))
+print(int(num2 > num1))
