100865ed241fa19bc462d6987442467e00138f4f
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-07 10:51:11 +0900

    Title: 두 사람, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250307/두 사람/two-person.py
@@ -0,0 +1,14 @@
+p1 = input()
+p1 = p1.split(" ")
+
+age1 = int(p1[0])
+sex1 = str(p1[1])
+
+p2 = input()
+p2 = p2.split(" ")
+
+age2 = int(p2[0])
+sex2 = str(p2[1])
+
+if (age1 >= 19 and sex1 == "M") or (age2 >= 19 and sex2 == "M"):
+    print(f"1")
