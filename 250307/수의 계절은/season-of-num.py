01a60efa8675b41da9d46a6c8552c2c16f376e1b
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-07 10:41:08 +0900

    Title: 수의 계절은, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250307/수의 계절은/season-of-num.py
@@ -0,0 +1,12 @@
+month = int(input())
+
+if month <= 2:
+    print(f"Winter")
+elif month <= 5:
+    print(f"Spring")
+elif month <= 8:
+    print(f"Summer")
+elif month <= 11:
+    print(f"Fall")
+else:
+    print(f"Winter")
