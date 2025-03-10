2f2fa648c6b1c688be3ac7e522464dbdceaf49b3
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-10 17:42:44 +0900

    Title: 일 수 구하기, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250310/일 수 구하기/number-of-days-in-month.py
+++ 250310/일 수 구하기/number-of-days-in-month.py
@@ -2,7 +2,7 @@ n = int(input())
 
 if n == 2:
     print(f"28")
-else if n == 1 or n == 3 or n == 5 or n == 7 or n == 8 or n == 10 or n == 12
+else if n == 1 or n == 3 or n == 5 or n == 7 or n == 8 or n == 10 or n == 12:
     print(f"31")
 else:
     print(f"30")
