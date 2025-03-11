b2e73342345888cd0b79235c98fca0c2fabbdd6a
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-11 18:31:18 +0900

    Title: 중앙값 구하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/중앙값 구하기/find-the-median.py
+++ 250311/중앙값 구하기/find-the-median.py
@@ -5,10 +5,10 @@ f = int(numbers[0])
 s = int(numbers[1])
 t = int(numbers[2])
 
-if (f >= s and f <= t) or (f >= s and f <= t):
+if (f >= s and f <= t) or (f >= t and f <= s):
     print(f"{f}")
-elif (s >= f and s <= t) or (s >= f and s <= t):
+elif (s >= f and s <= t) or (s >= t and s <= f):
     print(f"{s}")
-elif (t >= f and t <= s) or (t >= f and t <= s):
+elif (t >= f and t <= s) or (t >= s and t <= f):
     print(f"{t}")
 
