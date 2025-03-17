0d270b9fb06965040339ffc99631ef8925cad8d8
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-17 19:52:21 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250317/369 게임/369-game.py
+++ 250317/369 게임/369-game.py
@@ -2,19 +2,10 @@ number = int(input())
 start = 1
 
 while start <= number:
-    if start % 3 ==0:
-        print(f"0", end = " ")
-        start = start + 1
+    str_number = str(start)
+    # 3의 배수이거나 숫자에 3,6,9 포함 시
+    if (start % 3 == 0) or ('3' in str_number) or ('6' in str_number) or ('9' in str_number):
+        print("0", end=" ")
     else:
-        str_number = str(start)
-        for i in range(len(str_number)):
-            count = 0
-            if str_number[i] == '3' or str_number[i] == '6' or str_number[i] == '9':
-                count = count + 1
-        if count > 0:
-            print(f"0", end = " ")
-            start = start + 1  
-        else:
-            print(f"{start}", end = " ")
-            start = start + 1
-               
+        print(start, end=" ")
+    start += 1
