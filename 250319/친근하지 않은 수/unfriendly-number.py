46d01a72c4f0618425bec9cd0d4d25f39325608d
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-19 11:20:04 +0900

    Title: 친근하지 않은 수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250319/친근하지 않은 수/unfriendly-number.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+cnt = 0
+for i in range(N+1):
+    if (i+1) % 2 == 0:
+        cnt = cnt + 1
+        continue
+    elif (i+1) % 3 == 0 :
+        cnt = cnt + 1
+        continue
+    elif (i+1) % 5 == 0 :
+        cnt = cnt + 1
+        continue
+
+print(f"{(N) - cnt}")
