d7bdb9d31a28de3b9fcbedb37c23eee927f9dacc
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-21 10:39:42 +0900

    Title: 출석 부르기 2, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250321/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    number = int(input())
+
+    if number == 1:
+        print(f"John")
+    elif number == 2:
+        print(f"Tom")
+    elif number == 3:
+        print(f"Paul")
+    elif number == 4:
+        print(f"Sam")
+    else:
+        print(f"Vacancy")
+        break
