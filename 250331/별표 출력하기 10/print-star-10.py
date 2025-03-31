874ba8a65a3ada4d4ed1eb5523fe0c2a7ec71a46
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-03-31 21:32:29 +0900

    Title: 별표 출력하기 10, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250331/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+first = 1
+second = n
+
+for i in range(1,2*n+1):
+
+    if i % 2 != 0:
+        for j in range(first):
+            print(f"* ", end = "")
+        first = first + 1
+        print()
+    
+    elif i%2 == 0:
+        for k in range(second):
+            print(f"* ", end = "")
+        second = second - 1
+        print()
