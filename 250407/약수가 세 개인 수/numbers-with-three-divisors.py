bc7223580a9882c07a7c009ed82b3d43b9b5dc84
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-04-07 11:03:34 +0900

    Title: 약수가 세 개인 수, Time: 48ms, Memory: 17MB, Status: Passed - Codetree

--- 250407/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 250407/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -1,4 +1,7 @@
-start, end = map(int, input().split())
+arr = input().split(" ")
+
+start = int(arr[0])
+end = int(arr[1])
 
 # Please write your code here.
 
@@ -8,7 +11,7 @@ for i in range(start, end+1):
     my_count = 0
     for j in range(1,i+1):
         if i % j == 0:
-            my_count = my_count + j
+            my_count = my_count + 1
 
     if my_count == 3:
         count = count + 1
