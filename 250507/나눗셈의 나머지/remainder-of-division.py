f2d242053e034059f8328f99685ead9a22891c39
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-05-07 14:56:29 +0900

    Title: 나눗셈의 나머지, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250507/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+A = arr[0]
+B = arr[1]
+
+num = [0] * A
+while A >= 1:
+    num[A%B] = num[A%B] + 1
+    A = int(A / B)
+
+sum = 0
+for i in range (len(num)):
+    if num[i] != 0:
+        sum = sum + num[i]**2;
+
+print(f"{sum}")
