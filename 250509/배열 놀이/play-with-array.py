1a8adbdf0584ce33fc4cca00dbc458570d622b09
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-05-09 15:25:47 +0900

    Title: 배열 놀이, Time: 87ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250509/배열 놀이/play-with-array.py
+++ 250509/배열 놀이/play-with-array.py
@@ -2,11 +2,13 @@ arr_first = list(map(int, input().split()))
 arr_second = list(map(int, input().split()))
 
 for i in range(arr_first[0]):
-
     arr = list(map(int, input().split()))
 
     if arr[0] == 1:
-        print(f"{arr_second[arr[1]-1]}")
+        if 1 <= arr[1] <= len(arr_second):
+            print(f"{arr_second[arr[1]-1]}")
+        else:
+            print("Invalid index")
 
     elif arr[0] == 2:
         for j in range(len(arr_second)):
@@ -17,6 +19,8 @@ for i in range(arr_first[0]):
             print(f"0")
 
     elif arr[0] == 3:
-        for k in range(arr[1]-1 , arr[2]):
-            print(f"{arr_second[k]}", end = " ")
-        print()  
+        start = max(arr[1] - 1, 0)
+        end = min(arr[2], len(arr_second))
+        for k in range(start, end):
+            print(f"{arr_second[k]}", end=" ")
+        print()
