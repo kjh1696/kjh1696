8cb7e0661f6a804fc2060314d5da397396f4498e
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-05-09 15:16:39 +0900

    Title: 배열 놀이, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250509/배열 놀이/play-with-array.py
@@ -0,0 +1,22 @@
+arr_first = list(map(int, input().split()))
+arr_second = list(map(int, input().split()))
+
+for i in range(arr_first[0]):
+
+    arr = list(map(int, input().split()))
+
+    if arr[0] == 1:
+        print(f"{arr_second[arr[1]-1]}")
+
+    elif arr[0] == 2:
+        for j in range(len(arr_second)):
+            if arr[1] == arr_second[j]:
+                print(f"{j+1}")
+                break
+        else:
+            print(f"0")
+
+    elif arr[0] == 3:
+        for k in range(arr[1]-1 , arr[2]):
+            print(f"{arr_second[k]}", end = " ")
+        print()  
