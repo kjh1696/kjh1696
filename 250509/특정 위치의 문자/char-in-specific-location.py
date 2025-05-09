520d1409c4ceac594f6b8117ba8a0a2322c22885
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-05-09 10:38:12 +0900

    Title: 특정 위치의 문자, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250509/특정 위치의 문자/char-in-specific-location.py
@@ -0,0 +1,13 @@
+word = ['L', 'E', 'B', 'R', 'O', 'S']
+
+spell = input()
+
+for i in range (len(word)):
+    if word[i] == spell:
+        print(f"{i}")
+        break
+
+if i == (len(word)-1):
+    print(f"None")
+
+
