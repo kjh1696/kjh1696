a53376e8e4ace10c0e032d1bce56113a0ebd68ce
KIM_JAE_HONG <88581095+kjh1696@users.noreply.github.com>
2025-05-09 15:40:05 +0900

    Title: 배열 놀이, Time: 54ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250509/배열 놀이/play-with-array.py
+++ 250509/배열 놀이/play-with-array.py
@@ -1,10 +1,13 @@
-arr_first = list(map(int, input().split()))
-arr_second = list(map(int, input().split()))
+import sys
 
-for i in range(1,arr_first[0]):
+arr_first = list(map(int, sys.stdin.readline().split()))
+arr_second = list(map(int, sys.stdin.readline().split()))
 
-    arr = input()
-    arr = list(map(int, arr.split()))
+for i in range(arr_first[0]):
+    line = sys.stdin.readline()
+    if not line:
+        break  # 입력이 더 이상 없으면 탈출
+    arr = list(map(int, line.strip().split()))
 
     if arr[0] == 1:
         print(f"{arr_second[arr[1]-1]}")
@@ -17,8 +20,7 @@ for i in range(1,arr_first[0]):
         else:
             print(f"0")
 
-
     elif arr[0] == 3:
         for k in range(arr[1]-1 , arr[2]):
-            print(f"{arr_second[k]}", end = " ")
+            print(f"{arr_second[k]}", end=" ")
         print()
